f877ecf8d3ed300c45e8282b88ecfa488426a1b7
deva kumar singh <imdeva91@gmail.com>
2024-08-08 19:57:18 +0530

    add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { default: axios } = require("axios")
 const express = require("express")
-
+const cron = require('node-cron')
 const app = express()
 const port = 5000
 
@@ -10,3 +11,17 @@ res.send("hello world")
 app.listen(port,()=>{
     console.log(`server is runing on port ${port}`)
 })
+
+async function generateTargetReports(){
+    try {
+        const data=await axios.get('https://onlinecompiler-backend.onrender.com/user/test')
+        console.log('data',data)
+        
+    } catch (error) {
+        console.log('error',error)
+        
+    }
+}
+
+
+cron.schedule('*/30 * * * * *', generateTargetReports)
