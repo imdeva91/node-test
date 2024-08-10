698d118ac2f07f95d031933e03a606c424d58c41
deva kumar singh <imdeva91@gmail.com>
2024-08-10 21:06:11 +0530

    add

--- src/app.js
+++ src/app.js
@@ -13,9 +13,10 @@ app.use(express.json({limit:'50mb'}))
 app.use(cookieParser())
 
 // Add cors policies ..........
-app.use(cors({
-    origin:process.env.ORIGIN||['http://localhost:3000']
-}))
+app.use(cors())
+// app.use(cors({
+//     origin:process.env.ORIGIN||['http://localhost:3000']
+// }))
 
 
 
